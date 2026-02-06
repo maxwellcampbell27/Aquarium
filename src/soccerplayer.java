1d17f9f312a95b30a9a5f1c16478f9bac96f1bd7
maxwellcampbell27 <maxwell_campbell27@milton.edu>
2026-02-06 08:09:26 -0500

    Making Soccerfield

---
+++ src/soccerplayer.java
@@ -0,0 +1,30 @@
+public class soccerplayer {
+
+    public String name;                //holds the name of the hero
+    public int xpos;                //the x position
+    public int ypos;                //the y position
+    public int dx;                    //the speed of the hero in the x direction
+    public int dy;                    //the speed of the hero in the y direction
+    public int width;
+    public int height;
+    public boolean isAlive;
+
+    public soccerplayer(int pXpos, int pYpos) {
+        xpos = pXpos;
+        ypos = pYpos;
+        dx = 1;
+        dy = 0;
+        width = 60;
+        height = 60;
+        isAlive = true;
+
+
+
+    }
+
+    public void move() {
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+    }
+
+}
